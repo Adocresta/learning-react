6e9e76519f215a80d6b32e60ae4077b60b4c8f36
Onur <znygaga004@gmail.com>
2022-06-17 21:29:37 +0300

    useState with an object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,35 +3,65 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   const defaultTitle = "Please Enter your expense name.";
-  const [enteredTitle, setEnteredTitle] = useState(defaultTitle);
-  const [enteredAmount, setEnteredAmount] = useState("Free!");
-  const [selectedDate, setSelectedDate] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState(defaultTitle);
+  // const [enteredAmount, setEnteredAmount] = useState("Free!");
+  // const [selectedDate, setSelectedDate] = useState("");
+  // using objects as useState!
+  const [userInput, setUserInput] = useState({
+    enteredTitle: defaultTitle,
+    enteredAmount: "Free!",
+    selectedDate: "Please select a date.",
+  });
 
   const titleChangeHandler = (e) => {
-    setEnteredTitle(e.target.value);
-    !e.target.value && setEnteredTitle(defaultTitle);
+    // setEnteredTitle(e.target.value);
+    // !e.target.value && setEnteredTitle(defaultTitle);
+
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: e.target.value };
+    });
+    !e.target.value &&
+      setUserInput((prevState) => {
+        return { ...prevState, enteredTitle: defaultTitle };
+      });
   };
 
   const amountChangeHandler = (e) => {
-    setEnteredAmount(e.target.value);
-    !e.target.value && setEnteredAmount("Free!");
-    e.target.value === "0" && setEnteredAmount("Free!");
+    // setUserInput(e.target.value);
+    // !e.target.value && setEnteredAmount("Free!");
+    // e.target.value === "0" && setEnteredAmount("Free!");
+    const userInput = parseFloat(e.target.value);
+    console.log(userInput);
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: userInput };
+    });
+    !e.target.value | (userInput === 0) &&
+      setUserInput((prevState) => {
+        return { ...prevState, enteredAmount: "Free!" };
+      });
   };
 
   const dateChangeHandler = (e) => {
-    setSelectedDate(e.target.value);
-    !e.target.value && setSelectedDate("Please enter a date.");
+    // setUserInput(e.target.value);
+    // !e.target.value && setSelectedDate("Please select a date.");
+    setUserInput((prevState) => {
+      return { ...prevState, selectedDate: e.target.value };
+    });
+    !e.target.value &&
+      setUserInput((prevState) => {
+        return { ...prevState, selectedDate: "Please select a date." };
+      });
   };
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-          <label>Expense: {enteredTitle}</label>
+          <label>Expense: {userInput.enteredTitle}</label>
           <input id="test" type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label>Amount: ${enteredAmount}</label>
+          <label>Amount: ${userInput.enteredAmount}</label>
           <input
             type="number"
             min="0"
@@ -40,7 +70,7 @@ const ExpenseForm = () => {
           />
         </div>
         <div className="new-expense__control">
-          <label>Date: {selectedDate}</label>
+          <label>Date: {userInput.selectedDate}</label>
           <input
             type="date"
             min="2019-01-01"
