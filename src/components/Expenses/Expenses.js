4a425cf90141e412882f8d8927853f5b7c4937cf
Onur <znygaga004@gmail.com>
2022-06-23 16:29:57 +0300

    file management | readabity

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
-import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
-import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import { useState } from "react";
+
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "../ExpensesFilter/ExpensesList";
 
 function Expenses(props) {
   // Destructuring in Objects
@@ -15,28 +16,9 @@ function Expenses(props) {
     // !Dont create new array here👇 it uses old selectedYearValue instead pass year as parameter in jsx
   };
 
-  // filter stuff and map it directly no exstra step needed! filter won't effect original array just creates new one
-  const createExpenseItemArray = (expenses, _selectedYear) => {
-    const filteredExpenses = expenses.filter(
-      (expense) => expense.date.getFullYear().toString() === _selectedYear
-    );
-    console.log(filteredExpenses);
-    // checking if there is no data to show and inform the user
-    if (filteredExpenses.length === 0) {
-      return <p>No Expenses found.</p>;
-    } else {
-      return filteredExpenses.map((element) => {
-        return (
-          <ExpenseItem
-            title={element.title}
-            amount={element.amount}
-            date={element.date}
-            key={element.id}
-          />
-        );
-      });
-    }
-  };
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === selectedYearValue
+  );
 
   return (
     <Card className="expenses">
@@ -44,7 +26,7 @@ function Expenses(props) {
         selected={selectedYearValue}
         onYearSelected={yearFilterHandler}
       />
-      {createExpenseItemArray(expenses, selectedYearValue)}
+      <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
 }
---
+++ src/components/ExpensesFilter/ExpensesList.js
@@ -0,0 +1,33 @@
+import ExpenseItem from "../Expenses/ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  const { expenses } = props;
+  console.log(expenses);
+  // filter stuff and map it directly no exstra step needed! filter won't effect original array just creates new one
+  const expensesContent = (
+    <h2 className="expenses-list__fallback">No Expenses found.</h2>
+  );
+  let listCreatedDynamically = null;
+
+  if (expenses.length > 0) {
+    console.log("array is available", expenses);
+    listCreatedDynamically = expenses.map((element) => {
+      return (
+        <ExpenseItem
+          title={element.title}
+          amount={element.amount}
+          date={element.date}
+          key={element.id}
+        />
+      );
+    });
+  } else {
+    console.log("empty array");
+    return expensesContent;
+  }
+
+  return <ul className="expenses-list">{listCreatedDynamically}</ul>;
+};
+
+export default ExpensesList;
